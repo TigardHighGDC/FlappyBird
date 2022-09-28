86fb2828e79ffb573271d688f5ecf0351134d552
BrandonPacewic <pacewicbrandon@gmail.com>
2022-09-28 11:20:35 -0700

    Update to hello world

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,18 +1,16 @@
+// Copyright (c) TigardHighGDC
+// SPDX-License SPDX-License-Identifier: Apache-2.0
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+#if DEBUG
+        Debug.Log("Hello World!");
+#endif
     }
 }
